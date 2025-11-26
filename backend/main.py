eb70f23da0b9515a15787a8db7c52ca16056306c
koichi <111332153+koichi2426@users.noreply.github.com>
2025-11-26 23:29:04 +0900

    全て許可する。

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 # フロントエンドからのアクセスを許可するためのCORS設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
